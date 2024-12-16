211a0c28be9beda9af0e5ed604b68e5867a95edc
mrborghini <61702082+mrborghini@users.noreply.github.com>
2024-12-16 11:01:47 +0100

    Removed comments

--- scripts/stats_label.gd
+++ scripts/stats_label.gd
@@ -1,8 +1,5 @@
 extends RichTextLabel
 
 func _process(_delta: float) -> void:
-	# Format the score and time strings
 	var score_string: String = "Score: %d\n" % PlayerScores.score
-
-	# Update the display
 	self.text = score_string + PlayerScores.get_time_string()
